176e5f703281f63025ec0c1fcef912e55e9c248a
Om Navle <120503863+omnavle@users.noreply.github.com>
2026-07-27 16:26:11 +0530

    Time: 1 ms (99.90%) | Memory: 47 MB (27.49%) - LeetSync

---
+++ 412-fizz-buzz/fizz-buzz.java
@@ -0,0 +1,21 @@
+public class Solution {
+    public List<String> fizzBuzz(int n) {
+        List<String> result = new ArrayList<>();
+        for (int i = 1; i <= n; i++) {
+            if (i % 3 == 0 && i % 5 == 0) {
+                // Number is divisible by both 3 and 5
+                result.add("FizzBuzz");
+            } else if (i % 3 == 0) {
+                // Number is divisible by 3
+                result.add("Fizz");
+            } else if (i % 5 == 0) {
+                // Number is divisible by 5
+                result.add("Buzz");
+            } else {
+                // Number is not divisible by 3 or 5
+                result.add(Integer.toString(i));
+            }
+        }
+        return result;
+    }
+}
